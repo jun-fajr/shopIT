5784197c34bd5704b9387631a74bd898e150b53b
Jun al-Fajr <jun.fajr@gmail.com>
2021-08-17 20:33:42 +0700

    add jwt token

---
+++ backend/controllers/authController.js
@@ -0,0 +1,58 @@
+const User = require('../models/user')
+
+const ErrorHandler = require('../utils/errorHandler')
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
+
+// register a user > /api/v1/register
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+    const { name, email, password } = req.body
+    const user = await User.create({
+        name, 
+        email, 
+        password, 
+        avatar: {
+            public_id: 'avatars/kccvibpsuiusmwfepb3m',
+            url: 'sdsdd'
+        }
+    })
+
+    const token = user.getJwtToken()
+
+    res.status(201).json({
+        success: true,
+        token
+    })
+})
+
+// login user => v1/login
+exports.loginUser = catchAsyncErrors(async (req, res, next)=> {
+    const { email, password } = req.body
+    
+    // check if email and password are valid by user
+    if (!email||!password){
+        return next(new ErrorHandler('Please enter email & password', 400))
+    }
+
+    // finding user in db
+    const user = await User.findOne({email}).select('+password')
+
+    if (!user) {
+        return next(new ErrorHandler('invalid email or password', 401))
+    }
+
+    // check if password is correct or not
+    const isPasswordMatched = await user.comparePassword(password)
+
+    if(!isPasswordMatched){
+        return next(new ErrorHandler('invalid email or password',401))
+    }
+
+    const token = user.getJwtToken()
+
+    res.status(200).json({
+        success: true,
+        token
+    })
+
+})
+
---
+++ backend/models/user.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const validator = require('validator')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter your name'],
+        maxLength: [50, 'Product name cannot exceed 50 characters'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter your email'],
+        unique: true,
+        validate: [validator.isEmail, 'Please enter a valid email address'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter your password'],
+        minLength: [6, 'Your Password must be at least 6 characters'],
+        select: false
+    },
+    avatar: {
+        public_id: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    },
+    role: {
+        type: String,
+        default: 'user'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    resetPasswordToken: String,
+    resetPasswordExpired: Date
+})
+
+// encryptedPassword before saving user
+userSchema.pre('save', async function (next) {
+    if(!this.isModified('password')){
+        next()
+    }
+
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+// compare user password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+// return jwt token
+userSchema.methods.getJwtToken = function () {
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_TIME
+    })
+}
+
+module.exports = mongoose.model('User', userSchema)
