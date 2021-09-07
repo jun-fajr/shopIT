c152a24449cc4c286417238f0e79864a257cca7f
Jun al-Fajr <jun.fajr@gmail.com>
2021-09-07 11:40:48 +0700

    crud orders

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const server = app.listen(process.env.PORT, () => {
 
 // handle unhandled promise rejection
 process.on('unhandledRejection', err => {
-    console.log(`ERROR: ${err.message}`)
+    console.log(`ERROR: ${err.stack}`)
     console.log('Shutting down the server due to unhandled Promise rejection')
     server.close(()=> {
         process.exit(1)
