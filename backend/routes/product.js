70e3112575359bb03092484be4e76f964c699374
Jun al-Fajr <jun.fajr@gmail.com>
2021-09-07 19:20:14 +0700

    crud reviews

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const {getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct} = require('../controllers/productController')
+const {getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct, createProductReview, getProductReviews, deleteReview} = require('../controllers/productController')
 
 const {isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth')
 
@@ -11,5 +11,9 @@ router.route('/product/new').post(isAuthenticatedUser, authorizeRoles('admin'), 
 router.route('/admin/product/:id')
     .put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct)
     .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct)
+
+router.route('/review').put(isAuthenticatedUser, createProductReview)
+router.route('/reviews').get(isAuthenticatedUser, getProductReviews)
+router.route('/reviews').delete(isAuthenticatedUser, deleteReview)
 
 module.exports = router      
