6ab7a600b66b1ea7ed72d45b02956438201b2bfb
Jun al-Fajr <jun.fajr@gmail.com>
2021-09-11 21:42:33 +0700

    add custom title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
+import Header from "./components/layout/Header";
+import Home from "./components/Home";
+import Footer from "./components/layout/Footer";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Shop IT</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container container-fluid">
+          <Route path="/" component={Home} exact/>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
